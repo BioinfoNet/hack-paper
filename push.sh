5cd4f3f68174cff3eb362c661c2cc73f98c87f6c
Caleb Kipkurui <calebkibet88@gmail.com>
2018-08-21 18:54:17 +0300

    automate pdf push

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_paper_files() {
+  git add ./documents/*.pdf
+  git add ./documents/*.tex
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GIT_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+setup_git
+commit_paper_files
+upload_files
